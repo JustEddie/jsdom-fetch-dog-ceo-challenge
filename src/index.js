fc08637c642f88b9dd614b9731d537bc76877fb3
Eddie <skdpwls830@gmail.com>
2022-07-27 17:54:32 +1000

    figured out how to filter breeds with dropdown

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 console.log('%c HI', 'color: firebrick')
 
-// CHALLENGE 1
 
 fetch(imgUrl = "https://dog.ceo/api/breeds/image/random/4")
     .then(resp=> resp.json())
@@ -19,47 +18,29 @@ function renderImage(json) {
 
 
 
-// CHALLENGE 2
-function getBreeds(){
 const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 fetch(breedUrl)
 .then(resp => resp.json())
 .then(json=> {
     const breedList = document.getElementById('dog-breeds')
     const breeds = json.message;
+    const breedDropdown = document.querySelector('#breed-dropdown');
+
 
     for (const breed in breeds) {
         const node = document.createElement('li');
+
         node.innerText = breed;
-        breedList.appendChild(node);
-    }
-     
-})};
-
-// CHALLENGE 3 font colour of a particular li changes on click.
-
-function clickedColour(event) {
-    event.srcElement.style.color = 'red';
-}
-
-// CHALLENGE 4 filter breeds that start with a particular letter using a dropdown.
-document.addEventListener('DOMContentLoaded', () => {
- getBreeds();
-
- document.getElementById('breed-dropdown').addEventListener('change',event => {
-    console.log(event.target.value);
-    const breedList = document.getElementById('dog-breeds');
-    const breeds = [...breedList.children].filter(item => {
-        if (event.target.value === item.innerText[0]) {
-            return true;
+        if(node.innerText[0]===breedDropdown.value){
+            breedList.appendChild(node);
         }
-    });
-    for(const breed of breedList.children) {
-        if (breeds.includes(breed)) {
-            breed.style.display = 'list-item';
-        } else {
-            breed.style.display = 'none';
-        }
-    }
- })
-})
+        
+        
+    };
+    
+});
+
+//on change..... 
+
+
+
